725c1eaba0f18821539e5e520ce8e09a1466f827
Ryan O'Shaughnessy <ryan.oshag@gatech.edu>
2020-02-22 12:12:38 -0500

    Add initial login page with sign-in component.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import LoginPage from './pages/login-page/login-page.component';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/login' component={LoginPage} />
       </Switch>
     </div>
   );
