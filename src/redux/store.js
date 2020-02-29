15713f6aeb30b1ccd5e703bce1cd3cacd782b184
Ryan O'Shaughnessy <ryan.oshag@gatech.edu>
2020-02-29 16:23:01 -0500

    Add support for Redux dev tools in Chrome

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,14 @@
 
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
     rootReducer, 
-    applyMiddleware(...middlewares));
+    composeEnhancers(applyMiddleware(...middlewares)));
 
 export default store;
