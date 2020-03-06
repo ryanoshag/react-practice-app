1df4f5eba73743a5c22553b84ad55f92b8e2dd6c
Ryan O'Shaughnessy <ryan.oshag@gatech.edu>
2020-03-06 13:51:25 -0500

    Add redux-persist so that we can persist cart to local storage

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,21 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+// This is equivalent to using window.localStorage
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart-reducer';
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+    // User storage is being handled by firebase, so the only reducer that we want to persist is the cart.
+    whitelist: [ 'cart' ]
+};
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
